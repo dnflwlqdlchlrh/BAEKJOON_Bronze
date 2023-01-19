a14d94c1a32c3aad0056629c239abdfbc9f76bf4
허윤 <huryoun5025@gmail.com>
2023-01-19 17:27:40 +0900

    Bronze04

--- level_04/src/L4_16486.java
+++ level_04/src/L4_16486.java
@@ -2,10 +2,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class L4_16486 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int d1 = Integer.parseInt(br.readLine());
         int d2 = Integer.parseInt(br.readLine());
