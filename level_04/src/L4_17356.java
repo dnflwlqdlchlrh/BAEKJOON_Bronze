13c649be56db6eaf991d785b5dc2d2a215944719
허윤 <huryoun5025@gmail.com>
2023-01-19 17:31:15 +0900

    욱 제

--- level_04/src/L4_17356.java
+++ level_04/src/L4_17356.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class L4_17356 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
 		
 		double result = 1 / (1 + Math.pow(10, (B - A) / 400.0));
 		
