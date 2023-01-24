15b800f2c685c6aad8dbe99753c3484fd80236a8
USER1 <USER1@1.237.105.232>
2023-01-24 22:38:29 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ level_04/src/L4_24263.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class L4_24263 {
+
+//	  private void solution() throws Exception {
+//	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//	        System.out.println(br.readLine()+"\n1");
+//	    }
+
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//	        new  L4_24263().solution();
+	        System.out.println(br.readLine()+"\n1");
+	    	
+	    	
+	    }
+	}
