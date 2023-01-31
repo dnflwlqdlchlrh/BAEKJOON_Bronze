d81030e5afbdbc280ea9d423eb4aa6163816975f
USER1 <USER1@1.237.105.232>
2023-01-31 14:35:33 +0900

    Contemporary Art

---
+++ level_04/src/L4_16600.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class L4_16600 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long N = Long.parseLong(br.readLine());
+		
+		System.out.println(4 * Math.sqrt(N));
+
+	}
+
+}
